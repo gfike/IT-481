756f35d2fed6ead322620e9bea7d2a0a0f4b779b
Gail <gfike19@gmail.com>
2020-09-09 22:00:21 -0500

    working on next part of assignment, see link in file

--- unit2/main.php
+++ unit2/main.php
@@ -16,12 +16,47 @@ $stmt = $mysqli->prepare($query);
 $stmt->execute();
 $stmt->bind_result($custCount);
 $stmt->fetch();
-
+$stmt->close();
 return $custCount;
 
 }
 
-echo "Total number of customers: ".getCustCount($mysqli);
+// below is good for binding one array of reults to a variable
+// function getCustNames($mysqli) {
+//     $query = "SELECT CompanyName, ContactName FROM customers";
+//     $stmt = $mysqli->prepare($query);
+//     if (!$stmt->execute()) {
+//         echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+//    }
+   
+//    if (!($res = $stmt->get_result())) {
+//        echo "Getting result set failed: (" . $stmt->errno . ") " . $stmt->error;
+//    }
+   
+//    echo var_dump($res->fetch_all());
+// }
+
+// below runs multi query but puts it all into one variable
+function getCustNames($mysqli) {
+    $query = "SELECT CompanyName FROM customers;";
+    $query .= "SELECT ContactName FROM customers;";
+    if (!$mysqli->multi_query($query)) {
+        echo "Multi query failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+    
+    do {
+        if ($res = $mysqli->store_result()) {
+            var_dump($res->fetch_all(MYSQLI_ASSOC));
+            $res->free();
+        }
+    } while ($mysqli->more_results() && $mysqli->next_result());
+}
+
+
+//TODO https://www.w3schools.com/php/func_mysqli_multi_query.asp
+
+// echo "Total number of customers: ".getCustCount($mysqli);
+getCustNames($mysqli);
 
 $mysqli->close();
 ?>
