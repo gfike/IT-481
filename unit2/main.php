50963432b3827257fcfa1bb4c9fba556efb08b85
Gail <gfike19@gmail.com>
2020-09-09 21:35:21 -0500

    COMPLETE FIRST ITEM OF ASSIGNMENTgit add .

--- unit2/main.php
+++ unit2/main.php
@@ -3,6 +3,25 @@ $mysqli = new mysqli("localhost:8889", "northwind", "password", "northwind");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-echo $mysqli->host_info . "\n";
+// echo $mysqli->host_info . "\n";
 
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+function getCustCount($mysqli) {
+    $query = "SELECT COUNT(CustomerID) FROM customers";
+$stmt = $mysqli->prepare($query);
+$stmt->execute();
+$stmt->bind_result($custCount);
+$stmt->fetch();
+
+return $custCount;
+
+}
+
+echo "Total number of customers: ".getCustCount($mysqli);
+
+$mysqli->close();
 ?>
