9cec1b2234dd8f2f489941e2ae2978d979fbbd62
gfike19 <gfike19@gmail.com>
2020-09-13 19:27:46 -0500

    had to alter connection string to work on laptop

--- main.php
+++ main.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost:8889", "northwind", "password", "northwind");
+$mysqli = new mysqli("localhost:3307", "northwind", "password", "northwind");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
