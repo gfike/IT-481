c94be243b9f444e0cedc2b71d55dbde6449e3f11
Gail <gfike19@gmail.com>
2020-09-21 16:22:54 -0500

    issue with user accts seems to be they are not using northwind schema

--- genGroupsUsers.sql
+++ genGroupsUsers.sql
@@ -1,24 +1,22 @@
-CREATE ROLE "SalesRole";
-CREATE ROLE "HRRole";
-CREATE ROLE "CEORole";
+CREATE ROLE "SalesRole"@"localhost";
+CREATE ROLE "HRRole"@"localhost";
+CREATE ROLE "CEORole"@"localhost";
 
-GRANT SELECT ON northwind.orders TO "SalesRole" @"localhost";
-GRANT SELECT ON northwind.custoemrs TO "SalesRole" @"localhost";
-GRANT SELECT ON northwind.employees TO "HRRole" @"localhost";
-GRANT SELECT ON northwind.Orders TO "CEORole" @"localhost";
-GRANT SELECT ON northwind.Customers TO "CEORole" @"localhost";
-GRANT SELECT ON northwind.employees TO "CEORole" @"localhost";
-
-CREATE USER "User_CEO"@"localhost" IDENTIFIED BY "password";
-CREATE USER "User_HR"@"localhost" IDENTIFIED BY "password";
-CREATE USER "User_Sales"@"localhost" IDENTIFIED BY "password";
+CREATE USER "User_CEO"@"localhost" IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT CEORole to "User_CEO"@"localhost";
+CREATE USER "User_HR"@"localhost" IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT HRRole to "User_HR"@"localhost";
+CREATE USER "User_Sales"@"localhost" IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT SalesRole to "User_Sales"@"localhost";
 
 -- ALTER USER User_CEO@'localhost' IDENTIFIED BY 'New-Password-Here';
 -- alter user User_CEO @ 'localhost' IDENTIFIED BY "password";
 -- alter user User_HR IDENTIFIED BY "password";
 -- alter user User_Sales IDENTIFIED BY "password";
 
-GRANT "CEORole" to "User_CEO"@"localhost";
-GRANT HRRole to User_HR;
-GRANT SalesRole to User_Sales;
-
+-- GRANT SELECT ON northwind.orders TO SalesRole @"localhost";
+-- GRANT SELECT ON northwind.custoemrs TO SalesRole @"localhost";
+-- GRANT SELECT ON northwind.employees TO HRRole @"localhost";
+-- GRANT SELECT ON northwind.Orders TO CEORole @"localhost";
+-- GRANT SELECT ON northwind.Customers TO CEORole @"localhost";
+-- GRANT SELECT ON northwind.employees TO CEORole @"localhost";
--- login/login.php
+++ login/login.php
@@ -6,7 +6,7 @@ $server = $_POST["server"];
 $db = $_POST["db"];
 $user = $_POST["user"];
 $pwd = $_POST["pwd"];
-
+try {
 $mysqli = new mysqli($server, $user, $pwd, $db);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
@@ -17,7 +17,11 @@ if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
+} catch (Exception $e) {
+    echo $e->getMessage()."\n";
+}
+ echo "Welcome!";
 
-header("Location: ../tableSelector/tableSelector.html");
+// header("Location: ../tableSelector/tableSelector.html");
 
 ?>
