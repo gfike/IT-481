623f200c77f1345cc4439398e1cc573822c59d36
Gail <gfike19@gmail.com>
2020-09-21 21:33:24 -0500

    got user ids to work, roles don't though for unknown reason

--- genGroupsUsers.sql
+++ genGroupsUsers.sql
@@ -1,22 +1,26 @@
-CREATE ROLE "SalesRole"@"localhost";
-CREATE ROLE "HRRole"@"localhost";
-CREATE ROLE "CEORole"@"localhost";
+USE northwind;
+CREATE USER "User_Sales"@"localhost" IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT SELECT ON northwind.orders TO "User_Sales" @"localhost";
+GRANT SELECT ON northwind.customers TO "User_Sales" @"localhost";
+
+CREATE USER "User_HR"@"localhost" IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT SELECT ON northwind.employees TO "User_HR" @"localhost";
 
 CREATE USER "User_CEO"@"localhost" IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT CEORole to "User_CEO"@"localhost";
-CREATE USER "User_HR"@"localhost" IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT HRRole to "User_HR"@"localhost";
-CREATE USER "User_Sales"@"localhost" IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT SalesRole to "User_Sales"@"localhost";
+GRANT SELECT ON northwind.Orders TO User_CEO @"localhost";
+GRANT SELECT ON northwind.Customers TO User_CEO  @"localhost";
+GRANT SELECT ON northwind.employees TO User_CEO  @"localhost";
 
--- ALTER USER User_CEO@'localhost' IDENTIFIED BY 'New-Password-Here';
--- alter user User_CEO @ 'localhost' IDENTIFIED BY "password";
--- alter user User_HR IDENTIFIED BY "password";
--- alter user User_Sales IDENTIFIED BY "password";
-
--- GRANT SELECT ON northwind.orders TO SalesRole @"localhost";
--- GRANT SELECT ON northwind.custoemrs TO SalesRole @"localhost";
--- GRANT SELECT ON northwind.employees TO HRRole @"localhost";
+-- below code doesn't work
+-- CREATE ROLE "HRRole"@"localhost";
+-- CREATE ROLE "SalesRole"@"localhost";
+-- CREATE ROLE "CEORole"@"localhost";
+-- GRANT "CEORole" to "User_CEO"@"localhost";
+-- GRANT SalesRole to "User_Sales"@"localhost";
+-- GRANT HRRole to "User_HR"@"localhost";
+-- GRANT SELECT ON northwind.orders TO "SalesRole" @"localhost";
+-- GRANT SELECT ON northwind.custoemrs TO "SalesRole" @"localhost";
 -- GRANT SELECT ON northwind.Orders TO CEORole @"localhost";
 -- GRANT SELECT ON northwind.Customers TO CEORole @"localhost";
 -- GRANT SELECT ON northwind.employees TO CEORole @"localhost";
+-- GRANT "CEORole" to "User_CEO"@"localhost";
